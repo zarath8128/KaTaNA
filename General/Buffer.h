e317435f2d3ec52fccb4e689bc174d95bef304c0
Mamoru OKAMOTO <okamoto@apple07.(none)>
2014-08-18 16:07:02 +0900

    add class SubBuffer

--- General/Buffer.h
+++ General/Buffer.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <type_traits>
 #include "Range.h"
+#include "Utility.h"
 
 namespace KaTaNA
 {
@@ -16,7 +17,27 @@ namespace KaTaNA
 		{
 			virtual ~IBuffer(){}
 			virtual T &operator[](unsigned int i) = 0;
-			virtual const T &operator[](unsigned int i) const = 0;
+			virtual T operator[](unsigned int i) const = 0;
+			virtual unsigned int Size() const = 0;
+		};
+
+		template<class T>
+		class SubBuffer
+			:public IBuffer<T>
+		{
+			IBuffer<T> &buf;
+		public:
+			const unsigned int offset, N;
+			SubBuffer(unsigned int offset, unsigned int N, IBuffer<T> &buf)
+				:buf(buf), offset(offset), N(N)
+			{
+				assert(offset + N < buf.Size());
+			}
+
+			T &operator[](unsigned int i){return assert(i < N), buf[offset + i];}
+			T operator[](unsigned int i) const{return assert(i < N), buf[offset + i];}
+			unsigned int Size() const{return N;}
+			unsigned int Next() const{return offset + Next;}
 		};
 
 		template<class T>
@@ -39,17 +60,51 @@ namespace KaTaNA
 			Array &operator=(const Array &a)
 			{
 				assert(N == a.N);
-				for(auto x : Forall(a))
-					buf[x.i] = x;
+				for(unsigned int i = 0; i < N; ++i)
+					buf[i] = a[i];
 				return *this;
 			}
 
 			constexpr operator T*() const {return buf;}
 			T &operator[](unsigned int i){return assert(i < N), buf[i];}
-			const T &operator[](unsigned int i) const{return assert(i < N), buf[i];}
-
+			T operator[](unsigned int i) const{return assert(i < N), buf[i];}
+			unsigned int Size() const {return N;}
 		};
 
+/*		template<class T>
+		class VLArray
+			:public IBuffer<T>
+		{
+			mutable T *buf;
+			unsigned int n;
+
+			void resize(unsigned int new_size)
+			{
+				T *new_buf = new T[new_size];
+				for(unsigned int i = 0; i < Min(N, new_size); ++i)
+					new_buf[i] = buf[i];
+				delete [] buf;
+				buf = new_buf;
+			}
+
+		public:
+			unsigned int const &N = n;
+
+			T &operator[](unsigned int i)
+			{
+				if(!(i < N))
+					DBG_WRITE("resize %d to %d", N, 2*N), resize(2*N);
+				return buf[i];
+			}
+
+			const T &operator[](unsigned int i) const
+			{
+				if(!(i < N))
+					DBG_WRITE("resize %d to %d", N, 2*N), resize(2*N);
+				return buf[i];
+			}
+		};
+*/
 		template<class T>
 		std::ostream &operator<<(std::ostream &dest, const Array<T> &a)
 		{
--- General/test.cpp
+++ General/test.cpp
@@ -1,11 +1,16 @@
 #include "Debug.h"
 #include "Buffer.h"
+#include "Utility.h"
 
 using namespace KaTaNA::General;
 
 struct A
 {
-	virtual void say(){std::cout << "Hello A!\n";}
+	virtual void say(int a = 0){std::cout << "Hello A!\n";
+		DBG_WRITE();
+		assert(0);
+	ASSERT(0);
+	}
 };
 struct B
 	:public A
@@ -13,11 +18,27 @@ struct B
 	void say(){std::cout << "Hello B!\n";}
 };
 
+struct C
+{
+	~C(){DBG_WRITE("destructed");}
+};
+
+const C& f(const C& c)
+{
+	DBG_WRITE();
+	return c;
+}
+
 int main()
 {
-	A aa;
-	B bb;
-	Clone<A> a(std::move(bb));
-	a->say();
+	EXPR_VAL(Min(10, 3, 1, 2));
+	Array<double> buffer(20);
+	SubBuffer<double> buf(10, 5, buffer);
+	for(int i = 0; i < 5; ++i)
+		buf[i] = i + 20;
+	for(int i = 0; i < 20; ++i)
+		std::cout << i << ":" << buffer[i] << std::endl;
+//	assert(0);
+//	A().say();
 	return 0;
 }
