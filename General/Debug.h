2b20751c018338799ae736e9af218220d9bfa07b
Mamoru OKAMOTO <okamoto@apple07.(none)>
2014-08-18 16:06:27 +0900

    modify DBG_WRITE, format is now like assert and printf

--- General/Debug.h
+++ General/Debug.h
@@ -2,6 +2,7 @@
 #define ZARATH_KATANA_GENERAL_DEBUG_H
 
 #ifdef __cplusplus
+#include <cassert>
 #include <cerrno>
 #include <cstring>
 #include <cstdio>
@@ -10,21 +11,52 @@
 #include <cxxabi.h>
 #include <utility>
 #else
+#include <assert.h>
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
 #endif
 
+//*-----------------------form GNU C Library-----------------------*//
+#ifndef __GNUC_PREREQ
+#  if defined __GNUC__ && defined __GNUC_MINOR__
+#    define __GNUC_PREREQ(maj, min) ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+#  else
+#    define __GNUC_PREREQ(maj, min) 0
+#  endif
+#endif
+
+#ifndef __ASSERT_FUNCTION
+#  if defined __cplusplus ? __GNUC_PREREQ (2, 6) : __GNUC_PREREQ (2, 4)
+#    define __ASSERT_FUNCTION    __PRETTY_FUNCTION__
+#  else
+#    if defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
+#      define __ASSERT_FUNCTION    __func__
+#    else
+#      define __ASSERT_FUNCTION    ((__const char *) 0)
+#    endif
+#  endif
+#endif
+//*----------------------------------------------------------------*//
+
+#define PRINT_HERE(cstream) fprintf(cstream, "%s:%d: %s: ", __FILE__, __LINE__, __ASSERT_FUNCTION)
+
 #ifndef NDEBUG
-#define DBG_WRITE(...) fprintf(stderr, "%s:%d In function '%s'", __FILE__, __LINE__, __func__), fprintf(stderr, "  "  __VA_ARGS__), fprintf(stderr, "\n")
+#  define ASSERT(expr, ...) {if(!(expr)) PRINT_HERE(stderr), fprintf(stderr, "Assertion `" #expr "' failed. " __VA_ARGS__), fprintf(stderr, "\n"), abort();}while(0);
 #else
-#define DBG_WRITE(...)
+#  define ASSERT(expr, ...)
 #endif
 
 #ifndef NDEBUG
-#define ERR_CHECK(expr) do{errno = 0; int err_buf; expr; if((err_buf = errno)) DBG_WRITE("%s", strerror(err_buf)); else DBG_WRITE("No error detected");}while(0)
+#  define DBG_WRITE(...) PRINT_HERE(stderr), fprintf(stderr, " "  __VA_ARGS__), fprintf(stderr, "\n")
 #else
-#define ERR_CHECK(expr) do{expr;}while(0)
+#  define DBG_WRITE(...)
+#endif
+
+#ifndef NDEBUG
+#  define ERR_CHECK(expr) do{errno = 0; int err_buf; expr; if((err_buf = errno)) DBG_WRITE("%s", strerror(err_buf)); else DBG_WRITE("No error detected");}while(0)
+#else
+#  define ERR_CHECK(expr) do{expr;}while(0)
 #endif
 
 #ifdef __cplusplus
